0c155f1954f3efad3d04cba420061417e1a1e7f0
Srabon Nath <sknath373@gmail.com>
2023-01-30 23:11:13 -0500

    final

--- audio.js
+++ audio.js
@@ -1,3 +1,4 @@
+//all the strings
 let string_EL = document.getElementById("string_EL");
 let string_A = document.getElementById("string_A");
 let string_D = document.getElementById("string_D");
@@ -5,6 +6,7 @@ let string_G = document.getElementById("string_G");
 let string_B = document.getElementById("string_B");
 let string_EH = document.getElementById("string_EH");
 
+//each audio for each string since each has a different note
 let EL = document.getElementById("EL");
 let A = document.getElementById("A");
 let D = document.getElementById("D");
@@ -12,6 +14,7 @@ let G = document.getElementById("G");
 let B = document.getElementById("B");
 let EH = document.getElementById("EH");
 
+//event listener for each string so that the guitar simulates an actual guitar
 string_EL.addEventListener("mouseover", function() {
     EL.play();
 });
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//get arrays of all cards and the invisible text
 let cards = document.getElementsByClassName("card");
 let hiddentexts = document.getElementsByClassName("side_2");
 
+//an array that will link each card with its text
 let cards_and_hiddentexts = [];
 
+//for loop to populate the array
 for(let i = 0; i<cards.length; i++) {
     cards_and_hiddentexts.push({
         key: cards[i],
@@ -10,6 +13,7 @@ for(let i = 0; i<cards.length; i++) {
     })
 }
 
+//for loop that makes the inivisble text visible when mouse goes over
 for(let i = 0; i<cards_and_hiddentexts.length; i++) {
     let card = cards_and_hiddentexts[i].key;
     let hiddentext = cards_and_hiddentexts[i].value;
